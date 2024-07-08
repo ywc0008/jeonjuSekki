7f62676f51abf84f59d14d11a64eff1ba72d6240
wzz <ywc0008@naver.com>
2024-07-08 15:36:52 +0900

    supabase client 설정

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,61 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "@/types/supabase";
+import { createBrowserClient, createServerClient } from "@supabase/ssr";
+import { getCookie, setCookie } from "cookies-next";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
+// Browser client
 export const createSupabaseBrowserClient = () =>
-  createBrowserClient(
+  createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+
+// - ServerActions, RouterHandler
+export const createServerSideClient = async (serverComponent = false) => {
+  const cookieStore = cookies();
+
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (key) => cookieStore.get(key)?.value,
+        set: (key, value, options) => {
+          if (serverComponent) return;
+          cookieStore.set(key, value, options);
+        },
+        remove: (key, options) => {
+          if (serverComponent) return;
+          cookieStore.set(key, "", options);
+        },
+      },
+    }
+  );
+};
+// - RSC
+export const createServerSideClientRSC = async () => {
+  return createServerSideClient(true);
+};
+
+// - Middleware
+export const createServerSideMiddleware = async (
+  req: NextRequest,
+  res: NextResponse
+) => {
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (key) => getCookie(key, { req, res }),
+        set: (key, value, options) => {
+          setCookie(key, value, { req, res, ...options });
+        },
+        remove: (key, options) => {
+          setCookie(key, "", { req, res, ...options });
+        },
+      },
+    }
+  );
+};
