5caa29b2734491d3dcf482f231a86ffcf66c1471
wzz <ywc0008@naver.com>
2024-07-17 23:12:00 +0900

    쇼츠 생성

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <div>
+      <div className="grid grid-cols-2">
         <section>
           <iframe
             width="560"
@@ -12,7 +12,17 @@ export default function Home() {
             allowFullScreen
           ></iframe>
         </section>
-        <section></section>
+        <section className="grid grid-cols-2 grid-rows-2">
+          <iframe
+            width="200"
+            height="350"
+            src="https://www.youtube.com/embed/3hgZaeiWV4U"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="YouTube video player"
+          ></iframe>
+        </section>
       </div>
     </div>
   );
